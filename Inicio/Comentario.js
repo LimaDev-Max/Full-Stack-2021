12f576663c3231ed1844ce62920bceec0f5abdaf
Fabio Lima <fabiolimadesenvolvedor@gmail.com>
2021-09-27 15:20:02 -0300

    web

---
+++ Inicio/Comentario.js
@@ -0,0 +1,14 @@
+// Console.log é usado para exibir algo na tela do computador!
+console.log('Passo #01');
+console.log('Passo #02'); // Mais um exemplo de console.log
+// console.log('Passo #03');
+
+
+/*
+ * Esse é um
+ * comentario de 
+ * multiplicas
+ * linhas!
+ */
+
+console.log('Passo #03');
