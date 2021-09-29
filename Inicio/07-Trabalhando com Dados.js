1766df03d552098238a9998386e0b7a241009c6f
Fabio Lima <fabiolimadesenvolvedor@gmail.com>
2021-09-28 23:41:07 -0300

    Dados Js

---
+++ Inicio/07-Trabalhando com Dados.js
@@ -0,0 +1,15 @@
+ let preco = 19.90;
+ let desconto = 0.4;
+
+  //Exemplo 1
+ console.log(19.9 * 0.6);
+ console.log( preco * (1 - desconto));
+
+ //Exemplo 2
+ let precoComDesconto = preco * (1 - desconto);
+ console.log(precoComDesconto);
+
+ //Exemplo 3
+ let nome = "Caderno"; // Variavel String (Texto) -> sequencia de simbolos!
+ let categoria = "papelaria";
+ console.log("Produto: " + nome + "Categoria: " + categoria);
