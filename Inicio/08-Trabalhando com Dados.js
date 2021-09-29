d48a8b7030aeebcd15281c51ebd900eb7f5121fc
Fabio Lima <fabiolimadesenvolvedor@gmail.com>
2021-09-28 23:52:57 -0300

    Dados Js

---
+++ Inicio/08-Trabalhando com Dados.js
@@ -0,0 +1,15 @@
+//Exemplo 01
+let idade = 31;
+console.log(typeof -31);
+console.log(typeof idade)
+
+//Exemplo 02
+let salario = 1500.00;
+console.log(typeof salario);
+
+//Exemplo 03
+let estarChovendo = true;// Verdadeiro ou Facil!
+console.log(typeof estarChovendo);
+
+console.log( typeof "Teste");
+console.log(typeof 'Teste');
