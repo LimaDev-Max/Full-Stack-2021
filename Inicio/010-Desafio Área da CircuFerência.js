fcc1cbb59a903d3dc3c11eccee4cfbe9f2f0b936
Fabio Lima <fabiolimadesenvolvedor@gmail.com>
2021-09-29 00:18:29 -0300

    Dasafio Circurferência Js

---
+++ Inicio/010-Desafio Área da CircuFerência.js
@@ -0,0 +1,9 @@
+const PI = 3.141592;
+const raio = 10
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da àrea é:" + areaCirc + "m2.");
+
+//Exemple 02
+areaCirc = Math.PI * raio * raio;
+console.log("O valor da àrea é:" + areaCirc + "m2.");
